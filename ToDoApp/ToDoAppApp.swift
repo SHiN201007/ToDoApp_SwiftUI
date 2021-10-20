984b68090228b4fd6bddb21e27cf0ba393478c4d
SHiN201007 <shin1007m@gmail.com>
2021-10-20 10:33:16 +0900

    Initial Commit

---
+++ ToDoApp/ToDoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoAppApp.swift
+//  ToDoApp
+//
+//  Created by 松丸真 on 2021/10/20.
+//
+
+import SwiftUI
+
+@main
+struct ToDoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
